4ec71e8462e8ce484d1e70badb98bb9bb8e69887
John Landheer <john.landheer@gmail.com>
2017-12-25 11:03:55 +0100

    Samples eruit

--- sample/Studiekring.Business/Customers/Queries/CustomerAlreadyExistsException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Studiekring.Business.Customers.Queries
-{
-    public class CustomerAlreadyExistsException : Exception
-    {
-        public CustomerAlreadyExistsException(Guid customerId) : base(customerId.ToString()) { }
-    }
-}
